63864ff5792203f3d26f92fb0f7e9315f05c17f5
Adaiah Ohenewaah Edinam Tsorblewu <ohenewaahedinam804@gmail.com>
2025-08-06 13:25:21 +0000

    Remove .vs folder from tracking and add to .gitignore

---
+++ Inheritance&MethodOverriding/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Inheritance_And_Method_Overiding_Program
+{
+    public class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+        
+        public class Dog : Animal
+        {
+            public override void MakeSound()
+            {
+                Console.WriteLine("Bark");
+            }
+        }
+
+        public class Cat : Animal
+        {
+            public override void MakeSound()
+            {
+                Console.WriteLine("Meow");
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Animal genericAnimal = new Animal();
+                Animal dog = new Dog();
+                Animal cat = new Cat();
+
+                genericAnimal.MakeSound();
+                dog.MakeSound();
+                cat.MakeSound();
+            }
+        }
+       
+    }
+}
