bf369f9a772ffa4c3912c95b379cf781bf363933
Adaiah Ohenewaah Edinam Tsorblewu <ohenewaahedinam804@gmail.com>
2025-08-06 14:19:54 +0000

    Created an Interface Console Application that calls Bicycle and Car whenever the Interface method move is called

---
+++ Interfaces/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace InterfaceApplication
+{
+    public interface IMovable
+    {
+               void Move();
+    }
+    public class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving.");
+        }
+    }
+    public class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            IMovable myCar = new Car();
+            IMovable myBicycle = new Bicycle();
+            myCar.Move();
+            myBicycle.Move();
+        }
+    }
+}
