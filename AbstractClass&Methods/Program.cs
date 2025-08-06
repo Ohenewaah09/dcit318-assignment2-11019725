fc3ff44e64a3d199cb8f773970c670fb9c97a432
Adaiah Ohenewaah Edinam Tsorblewu <ohenewaahedinam804@gmail.com>
2025-08-06 13:58:59 +0000

    Add .NET 8.0 console application with shape classes

---
+++ AbstractClass&Methods/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Abstract_Class_Method
+{
+    public abstract class  Shape
+    {
+        public abstract double GetArea();
+    }
+
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public override double GetArea()
+        {
+           return Math.PI * radius *radius;
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        private double width;
+        private double height;
+
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public override double GetArea()
+        {
+            return width * height;
+        }
+    }
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Shape circle = new Circle(8);
+            Console.WriteLine("Area of a circle: " + circle.GetArea());
+
+            Shape rectangle = new Rectangle(5, 10);
+            Console.WriteLine("Area of a rectangle: " + rectangle.GetArea());
+        }
+    }
+}
+
